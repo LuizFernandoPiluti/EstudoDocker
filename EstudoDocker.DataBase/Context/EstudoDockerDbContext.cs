ad8a053394478f624e38b8c082fb4b24ba2c46ad
Luiz Fernando Piluti <fpiluti@outlook.com>
2026-03-20 02:34:34 -0300

    criado base dados atravez do migrations

---
+++ EstudoDocker.DataBase/Context/EstudoDockerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EstudoDocker.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoDocker.DataBase.Context
+{
+    public class EstudoDockerDbContext:DbContext
+    {
+
+        // O construtor deve aceitar DbContextOptions<SeuContexto> e passá-lo para a classe base
+        public EstudoDockerDbContext(DbContextOptions<EstudoDockerDbContext> options)
+            : base(options)
+        {
+        }
+
+
+
+        public DbSet<Pesssoa> Pessoa{ get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(connectionString: "Server=Localhost,1433;Database=EstudoDocker;User Id=sa;Password=!Admin123;TrustServerCertificate=True");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+   
+    
+    }
+}
---
+++ EstudoDocker.DataBase/Migrations/20260320053000_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudoDocker.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
